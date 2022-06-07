36ebd708e1ae8fc8f8e736025513267931596384
Martin Cuello <martin30016@gmail.com>
2022-06-07 18:56:31 -0300

    added flatlist in PhotoList

--- components/PhotoList.js
+++ components/PhotoList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { FlatList, ScrollView, Text, View } from 'react-native';
 import axios from 'axios';
 import PhotoDetail from './PhotoDetail';
 
@@ -23,26 +23,49 @@ const PhotoList = (props) => {
     getPhotos();
   }, [])
 
-  const renderAlbums = () => {
-    return photos.map((photo) => (
-      <PhotoDetail
-        key={photo.title}
-        title={photo.title}
-        imageUrl={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-      />
-    ));
-  }
+  const renderPhoto = ({ item }) => (
+    <PhotoDetail
+      key={item.title}
+      title={item.title}
+      imageUrl={`https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`}
+    />
+  );
 
   return (
     (!photos ?
-      <View style={{ flex: 1 }}>
-        <Text>Loading...</Text>
-      </View>
+      <Text>Loading...</Text>
       :
       <View style={{ flex: 1 }}>
-        <ScrollView>{renderAlbums()}</ScrollView>
-      </View>)
-  )
+        <FlatList
+          data={photos}
+          renderItem={renderPhoto}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    )
+  );
+
+//   const renderPhotos = () => {
+//     return photos.map((photo) => (
+//       <PhotoDetail
+//         key={photo.title}
+//         title={photo.title}
+//         imageUrl={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+//       />
+//     ));
+//   }
+
+//   return (
+//     (!photos ?
+//       <View style={{ flex: 1 }}>
+//         <Text>Loading...</Text>
+//       </View>
+//       :
+//       <View style={{ flex: 1 }}>
+//         <ScrollView>{renderPhotos()}</ScrollView>
+//       </View>)
+//   )
+
 }
 
 export default PhotoList;
